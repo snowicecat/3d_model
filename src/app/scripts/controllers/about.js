87567efc04a296aaa149cb6fb25585403e5b0599
snowicecat <snowicecat@126.com>
2015-11-12 02:13:05 +0800

    Successfully Construct the Environment

---
+++ src/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name srcApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the srcApp
+ */
+angular.module('srcApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
